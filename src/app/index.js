06e989c6886d0b92aabbd94b9b6cdf8e67b013a9
yshar <yogesh.sharma@monsanto.com>
2018-04-06 09:40:30 -0500

    committing multiple components branch

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Home } from './components/Home';
+import { Header } from './components/Header';
 
 class App extends React.Component {
   render() {
@@ -7,7 +9,12 @@ class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-10 com-xs-offset-1">
-            <h1>React Js Wilkommen du</h1>
+            <Header />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 com-xs-offset-1">
+            <Home />
           </div>
         </div>
       </div>
