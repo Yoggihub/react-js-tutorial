6628808ba68cf8464b243175468afaa0de056d63
yshar <yogesh.sharma@monsanto.com>
2018-04-11 09:30:54 -0500

    This branch shows how to use react router using browserHistory

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,103 +1,11 @@
 import React from 'react';
 
 export class Home extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      age: props.startingAge,
-      status: 0,
-      homeLink: props.initialLinkName
-    };
-    setTimeout(() => {
-      this.setState({
-        status: 1
-      });
-    }, 3000);
-    console.log('Constructor');
-  }
-
-  // Execute immediately before initital render method
-  componentWillMount() {
-    console.log('Component will mount');
-  }
-
-  // Execute immediately after initital render method
-  componentDidMount() {
-    console.log('Component did mount');
-  }
-
-  // when components receive new props
-  componentWillReceiveProps(nextProps) {
-    console.log('Component will receive props', nextProps);
-  }
-
-  // before rendering, after receiving new props or
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('Should component update', nextProps, nextState);
-    // Don't update the view even the state is changed egs: status changed from 0 to 1 but the shouldComponent  related lyfecycles didn't changed.
-    // if (nextState.status === 1) {
-    //   return false;
-    // }
-    return true;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log('Component will update', nextProps, nextState);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('Component did update', prevProps, prevState);
-  }
-
-  componentWillUnmount(nextProps, nextState) {
-    console.log('component will Unmount', nextProps, nextState);
-  }
-
-  onChangeLink() {
-    this.props.changeLink(this.state.homeLink);
-  }
-
-  onHandleChange(event) {
-    this.setState({
-      homeLink: event.target.value
-    });
-  }
-  // This function will only update the status section. Turn on the Rendering section in dev tools in chrome and select Paint flashing to see changes.
-  incrementAge() {
-    this.setState({
-      age: this.state.age + 3
-    });
-  }
-
   render() {
     return (
       <div>
-        <p>This is a Home component</p>
-        {/* Comments: this.state.age is refering to the age stored in the state */}
-        <p>Your name is {this.props.name}, your age is {this.state.age}</p>
-        <p>Status: {this.state.status}</p>
-        <hr />
-        {/* Comments: this.incrementAge so this button will only execute when its been called */}
-        {/* Comments: onClick={this.incrementAge.bind(this)} is anothe rway to trigger events on onCLick */}
-        <button onClick={() => this.incrementAge()} className="btn btn-primary">Increment-Age</button>
-        <hr />
-        {/* We cannot do this here because greet is not a function. () => this.props.greet() */}
-        <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
-        <hr />
-        <input
-          type="text"
-          value={this.state.homeLink}
-          onChange={event => this.onHandleChange(event)}
-        />
-        <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
+        <h3>Home</h3>
       </div>
     );
   }
 }
-
-Home.propTypes = {
-  name: React.PropTypes.string,
-  age: React.PropTypes.number,
-  greet: React.PropTypes.func,
-  initialLinkName: React.PropTypes.string
-};
--- src/app/index.js
+++ src/app/index.js
@@ -1,60 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom';
 import { Home } from './components/Home';
-import { Header } from './components/Header';
+import { Root } from './components/Root';
+import { User } from './components/User';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      homeLink: 'Home',
-      homeMounted: true
-    };
-  }
-  onGreet() {
-    alert('Hallo!');
-  }
-
-  onChangeHomeMounted() {
-    this.setState({
-      homeMounted: !this.state.homeMounted
-    });
-  }
-  onChangeLinkName(newName) {
-    this.setState({
-      homeLink: newName
-    });
-  }
   render() {
-    let homeComp = '';
-    if (this.state.homeMounted) {
-      homeComp = (
-        <Home
-          name="Max"
-          startingAge={27}
-          greet={this.onGreet}
-          changeLink={this.onChangeLinkName.bind(this)}
-          initialLinkName={this.state.homeLink}
-        />
-      );
-    }
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-10 com-xs-offset-1">
-            <Header homeLink={this.state.homeLink} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-10 com-xs-offset-1" />
-          {homeComp}
-        </div>
-        <div className="row">
-          <div className="col-xs-10 com-xs-offset-1" />
-          <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un) Mount Home Component </button>
-        </div>
-      </div>
-
+      // <BrowserRouter>
+      //   <div>
+      //     <Route path="/" component={Root} />
+      //     <Route path="/user" component={User} />
+      //     <Route path="/home" component={Home} />
+      //   </div>
+      // </BrowserRouter>
+      // This will use the {this.props.children in the Root class and first load the header and then the childrens in the Root}
+      <Router history={browserHistory}>
+        <Root>
+          <Route path="/user" component={User} />
+          <Route path="/home" component={Home} />
+        </Root>
+      </Router>
     );
   }
 }
